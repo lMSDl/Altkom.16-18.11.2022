718bb3ddfe996895934d2d671e1e06d0d5a634d2
Paweł Łęczycki <pawel@msd.info.pl>
2022-11-16 10:25:11 +0100

    Budowa aplikacji ASP.NET Core

---
+++ WebApi/Program.cs
@@ -0,0 +1,42 @@
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.Use(async (httpContext, next) =>
+{
+
+    Console.WriteLine("Before Use1");
+    await next();
+    Console.WriteLine("After Use1");
+});
+app.Use(async (httpContext, next) =>
+{
+
+    Console.WriteLine("Before Use2");
+    await next();
+    Console.WriteLine("After Use2");
+});
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+//    }
+//}
